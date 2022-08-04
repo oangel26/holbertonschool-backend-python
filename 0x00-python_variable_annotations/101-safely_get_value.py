43b62486643632c481bd8d8fe7aad86204f41c3b
oangel26 <oa.angel26@gmail.com>
2022-08-04 16:52:39 -0500

    style

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,14 +8,13 @@ from typing import Mapping, Any, Union, TypeVar, NewType
 
 
 T = TypeVar('T')
+
+
 NoneType = NewType('NoneType', None)
 
 
 def safely_get_value(dct: Mapping, key: Any,
                      default: Union[T, NoneType]) -> Union[Any, T]:
-    """
-    Method that iterates over a dict
-    """
     if key in dct:
         return dct[key]
     else:
