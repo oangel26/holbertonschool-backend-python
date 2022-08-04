abeb9278acebe4f24b4bc3ba5a9038993d6aedfa
oangel26 <oa.angel26@gmail.com>
2022-08-04 13:18:54 -0500

    first commit

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Complex types - mixed list
+"""
+
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    type-annotated function sum_mixed_list which takes a list mxd_lst of
+    integers and floats and returns their sum as a float.
+    """
+    return float(sum(mxd_lst))
+
+
+if __name__ == "__main__":
+    print(sum_mixed_list.__annotations__)
+    mixed = [5, 4, 3.14, 666, 0.99]
+    ans = sum_mixed_list(mixed)
+    print(ans == sum(mixed))
+    print("sum_mixed_list(mixed) returns {} which is a {}"
+          .format(ans, type(ans)))
