4f624dc3f428fa5d10317627faa9f0c638ec1f76
oangel26 <oa.angel26@gmail.com>
2022-08-04 12:38:44 -0500

    style fixed

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
+from typing import List
 """
 Complex types - list of floats
 """
-from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
@@ -18,4 +18,5 @@ if __name__ == "__main__":
     floats_sum = sum_list(floats)
     print(floats_sum == sum(floats))
     print(sum_list.__annotations__)
-    print("sum_list(floats) returns {} which is a {}".format(floats_sum, type(floats_sum)))
+    print("sum_list(floats) returns {} which is a {}".format(floats_sum,
+                                                             type(floats_sum)))
