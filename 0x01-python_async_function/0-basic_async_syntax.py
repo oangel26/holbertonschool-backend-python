bebf2fc990fd108752a550f99b051e238aa29a0e
oangel26 <oa.angel26@gmail.com>
2022-08-08 15:13:28 -0500

    first commit

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -13,7 +13,9 @@ async def wait_random(max_delay: int = 10) -> float:
     that waits for a random delay between 0 and max_delay
     (included and float value) seconds and eventually returns it.
     """
-    return float(random.uniform(0, max_delay))
+    x = random.uniform(0, max_delay)
+    await asyncio.sleep(x)
+    return x
 
 
 if __name__ == "__main__":
