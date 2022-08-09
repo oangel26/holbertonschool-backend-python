f2ead217960c50225bd04a69f15a24a006e3b1cd
oangel26 <oa.angel26@gmail.com>
2022-08-09 13:30:36 -0500

    style fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,11 +3,16 @@
 Async Generator
 """
 
+
 import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> float:
+    """
+     coroutine will loop 10 times, each time asynchronously wait 1 second,
+     then yield a random number between 0 and 10.
+    """
     for i in range(10):
         await asyncio.sleep(1)
         x = random.uniform(0, 10)
