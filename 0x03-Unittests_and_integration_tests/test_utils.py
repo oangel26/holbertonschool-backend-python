bd89c90fe91fae7de008b0b5aaf1044557bba50d
oangel26 <oa.angel26@gmail.com>
2022-10-20 19:41:29 +0100

    Not my code

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,47 +1,82 @@
 #!/usr/bin/env python3
-"""
-Parameterize a unit test
+""" This project module contains the first unit test
+for utils.access_nested_map.
 """
 
 import unittest
-from utils import access_nested_map, get_json
-from parameterized import parameterized, parameterized_class
 from unittest.mock import patch
+from utils import access_nested_map, get_json, memoize
+from parameterized import parameterized
+
 
 class TestAccessNestedMap(unittest.TestCase):
-    """
-    Class wich tests unit test for utils.access_nested_map
+    """ TestAccessNestedMap inherits from unittest. TestCase to test
+    utils.access_nested_map.
     """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        """ Tests if access_nested_map method returns what it is supposed to.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
     @parameterized.expand([
         ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
+        ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, nested_map, path):
-        with self.assertRaises(KeyError) as cm:
-            access_nested_map(nested_map, path)
-
-        return cm
-
+        """ Tests if access_nested_map method raise an exception.
+        """
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
 
 
 class TestGetJson(unittest.TestCase):
-    """
-    Class which tests unit test for utils.get_son
+    """ TestAccessNestedMap inherits from unittest. TestCase to test
+    utils.get_json.
     """
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False}),
+        ("http://holberton.io", {"payload": False})
     ])
     def test_get_json(self, test_url, test_payload):
+        """ Tests if get_json method returns the expected result.
+        """
         with patch('requests.get') as mock:
             mock.return_value.json.return_value = test_payload
             self.assertEqual(get_json(test_url), test_payload)
             mock.assert_called_once()
+
+
+class TestMemoize(unittest.TestCase):
+    """ TestMemoize inherits from unittest. TestCase to test
+    utils.memoize.
+    """
+
+    def test_memoize(self):
+        """Tests if memoize returns the correct result after a_property
+        is called twice.
+        """
+        class TestClass:
+            """ TestClass to test utils.memoize."""
+
+            def a_method(self):
+                """Method that returns 42"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """Method that returns a_method"""
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock:
+            tc = TestClass()
+            self.assertEqual(tc.a_property, mock.return_value)
+            self.assertEqual(tc.a_property, mock.return_value)
+            mock.assert_called_once()
+
+
+if __name__ == "__main__":
+    unittest.main()
